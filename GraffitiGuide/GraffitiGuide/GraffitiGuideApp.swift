d178b89bc21f661c86c39b3a5547c4cf760b8080
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-03-26 14:06:16 +0000

    restart

---
+++ GraffitiGuide/GraffitiGuide/GraffitiGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraffitiGuideApp.swift
+//  GraffitiGuide
+//
+//  Created by Dylan O'Sullivan on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GraffitiGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
